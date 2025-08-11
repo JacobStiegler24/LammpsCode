3fe82f0fe612dd8e8b27daf14dde3745d3345cf2
JacobStiegler24 <jacob.stiegler@gmail.com>
2025-08-11 11:40:57 +0100

    changed corr k_r values. bandade fixed unfolding issue with corr data

--- scripts/bondReaderCORR.py
+++ scripts/bondReaderCORR.py
@@ -15,8 +15,8 @@ print('imported')
 # Free energy + standard error
 
 # Constants
-timestep = 0.0001 # lj
-nevery = 5 # how often data is dumped
+timestep = 0.005 # lj
+nevery = 100 # how often data is dumped
 indextime = timestep * nevery # time between each data point in picoseconds
 
 sigma = 22.5E-9 # m
@@ -44,6 +44,7 @@ def readfile():
 
     folder_num = len(folder_lst)
     counter = 1
+    
     for folder in folder_lst:
         # https://docs.python.org/3/library/re.html
         # https://docs.python.org/3/howto/regex.html#regex-howto
@@ -56,70 +57,77 @@ def readfile():
             continue
         
         seed = int(seed_match.group(1))
-        k_r = float(r_match.group(1))
-        k_t = float(t_match.group(1))
+        k_r_lmmps = float(r_match.group(1))
+        k_t_lmmps = float(t_match.group(1))
 
         folder_path = os.path.join(os.getcwd(), folder)
         os.chdir(folder_path)
 
         btype_lst = [[],[]]
         blen_lst = [[],[]]
-        
         with open('bondlengths.dump') as f:
             isItem = False
             counter2 = 0
             for line in f:
-                if isItem:
-                    btype_lst[counter2].append(line[0])
-                    blen_lst[counter2].append(line[2::].strip(' \n'))
-                    counter2 += 1
+                if 'ENTRIES c_btype c_bondlen' in line:
+                    isItem = True
+                    counter2 = 0
+                    continue
+                if not isItem:
+                    continue
                 if counter2 == 2:
                     isItem = False
                     counter2 = 0
-                if 'ENTRIES c_btype c_bondlen' in line:
-                    isItem = True
-        
+                    continue
+                parts = line.split()
+                if len(parts) >= 2:
+                    btype_lst[counter2].append(int(parts[0]))
+                    blen_lst[counter2].append(float(parts[1]))
+                counter2 += 1
+                
         bangle_lst = []
         with open('angles.dump') as f:
             isItem = False
             for line in f:
-                if isItem:
-                    isItem = False
-                    bangle_lst.append(line)
                 if 'ITEM: ENTRIES c_myAngle' in line:
                     isItem = True
+                    continue
+                if isItem:
+                    try:
+                        bangle_lst.append(float(line.strip()))
+                    except ValueError:
+                        pass
+                    isItem = False
+                
 
         mlen_arr = np.array(blen_lst, dtype=np.float64)[0, :] + np.array(blen_lst, dtype=np.float64)[1, :]
-        
+        blen_arr = np.array(blen_lst, dtype=float) 
         row = {
             "seed": seed,
-            "K_r": k_r*2,
-            "K_theta": k_t*2,
-            "types": np.array(btype_lst, dtype=int),
-            "lengths": mlen_arr,
-            "bond lengths": np.array(blen_lst),
-            "angle": np.array(bangle_lst, dtype=np.float64)
-        }
 
+            "K_r phys": k_r_lmmps*2,
+            "K_theta phys": k_t_lmmps*2,
+
+            "types": np.array(btype_lst, dtype=int),
+
+            "bond lengths": blen_arr,       
+            "monomer lengths": mlen_arr,       
+
+            "angle_deg": np.array(bangle_lst, dtype=float),
+        }
+        
         rows.append(row)
         os.chdir('..')
         print(f'{counter}/{folder_num} folders read')
         counter += 1
 
     df = pd.DataFrame(rows)
-    df1 = df.groupby(['K_r', 'K_theta']).agg({
-        'lengths': lambda x: np.concatenate(x.to_numpy()),
-        'angle': lambda x: np.concatenate(x.to_numpy()),
-        'types': lambda x: np.concatenate(x.to_numpy())
+    df1 = df.groupby(['K_r phys', 'K_theta phys']).agg({
+        'monomer lengths':       lambda s: np.concatenate(s.to_numpy()),
+        'angle_deg':     lambda s: np.concatenate(s.to_numpy()),
+        'types':         lambda s: np.concatenate(s.to_numpy()),
+        'bond lengths':  lambda s: np.concatenate([np.asarray(x, float) for x in s], axis=1),
     }).reset_index()
-
-    # df2 = df.groupby(['K_r', 'K_theta']).agg({
-    #     'K_r': 'first',
-    #     'K_theta': 'first',
-    #     'lengths': lambda x: np.mean(x),
-    #     'angle': lambda x: np.mean(x),
-    #     'types': lambda x: np.mean(x)
-    # }).reset_index()
     return df1
 
 def round_sig(x, sig=2):
@@ -134,12 +142,15 @@ def ZTheta(theta, k_t):
 def Zr_bond(r, k_r):
     exponent = (-k_r/(2*T_LJ))*(r-1)**2
     integrand = r**2*np.exp(exponent)
+
+    # Rmean = ( (3/(2*k_r))+1)*1 / ( (1/(2*k_r))+1 )
+    # STD=np.sqrt(1/(2*k_r))
+    # integrand = 1/np.sqrt(2*3.14*STD**2)*np.exp(-(r-Rmean)**2/(2*STD**2))
+
     return integrand
 
 def ExpectedThetaIntegral(theta, k_t):
-    exponent = (-k_t/(2*T_LJ))*(theta-np.pi)**2
-    integrand =  theta*np.sin(theta)*np.exp(exponent)
-    return integrand
+    return theta*ZTheta(theta,k_t)
 
 def ExpectedTheta(k_t):
     Z, Zerr = integrate.quad(ZTheta, 0, np.pi, args=(k_t,))
@@ -152,14 +163,10 @@ def ExpectedTheta(k_t):
     return expectedTheta, expectedTheta_err
 
 def ExpectedRIntegral_bond(r, k_r):
-    exponent = (-k_r/(2*T_LJ))*(r-1)**2
-    integrand = r**3*np.exp(exponent)
-    return integrand
+    return Zr_bond(r,k_r)*r
 
 def ExpectedRSqrIntegral_bond(r, k_r):
-    exponent = (-k_r/(2*T_LJ))*(r-1)**2
-    integrand = r**4*np.exp(exponent)
-    return integrand
+    return Zr_bond(r,k_r)*r**2
 
 def ExpectedR(k_r):
     Z, Zerr = integrate.quad(Zr_bond, 0, np.inf, args=(k_r,))
@@ -175,8 +182,9 @@ def ExpectedRSqr(k_r):
     invZ = 1/Z
     invZerr = Zerr/(Z**2)
 
-    integral, err = integrate.quad(ExpectedRSqrIntegral_bond, 0, np.inf, args=(k_r,))
-    expectedR_sqr_new = invZ*integral*2 + 2*(invZ*integral)**2
+    integral1, err = integrate.quad(ExpectedRSqrIntegral_bond, 0, np.inf, args=(k_r,))
+    integral2, err = integrate.quad(ExpectedRIntegral_bond, 0, np.inf, args=(k_r,))
+    expectedR_sqr_new = invZ*integral1*2 + 2*(invZ*integral2)**2
 
     return expectedR_sqr_new
 
@@ -189,39 +197,42 @@ def VarR(k_r):
     return var
 
 def stats(data_df):
-    rt_pairs = data_df[['K_r', 'K_theta']].drop_duplicates().values
+    rt_pairs = data_df[['K_r phys', 'K_theta phys']].drop_duplicates().values
     
 
-    std_r = []
-    sem_std_r = []
-    mean_r = []
-    sem_r = []
+    std_mlen = []
+    sem_std_mlen = []
+    mean_mlen = []
+    sem_mlen = []
     mean_t = []
     sem_t = []
     std_t = []
     sem_std_t = []
+    mean_blen = []
 
     for k_r, k_t in rt_pairs:
         try:
-            std_r.append(np.std(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['lengths'].iloc[0]))
-            sem_std_r.append((np.std(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['lengths'].iloc[0]))/np.sqrt(len(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['lengths'].iloc[0])))
+            std_mlen.append(np.std(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['monomer lengths'].iloc[0]))
+            sem_std_mlen.append((np.std(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['monomer lengths'].iloc[0]))/np.sqrt(len(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['monomer lengths'].iloc[0])))
             
-            mean_r.append(np.mean(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['lengths'].iloc[0]))
-            sem_r.append((np.mean(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['lengths'].iloc[0]))/np.sqrt(len(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['lengths'].iloc[0])))
+            mean_mlen.append(np.mean(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['monomer lengths'].iloc[0]))
+            sem_mlen.append((np.mean(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['monomer lengths'].iloc[0]))/np.sqrt(len(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['monomer lengths'].iloc[0])))
 
-            std_t.append(np.std(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['angle'].iloc[0])*np.pi/180)
-            sem_std_t.append((np.std(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['angle'].iloc[0])*np.pi/180)/np.sqrt(len(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['angle'].iloc[0])))
+            std_t.append(np.std(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['angle_deg'].iloc[0])*np.pi/180)
+            sem_std_t.append((np.std(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['angle_deg'].iloc[0])*np.pi/180)/np.sqrt(len(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['angle_deg'].iloc[0])))
 
-            mean_t.append(np.mean(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['angle'].iloc[0])*np.pi/180)
-            sem_t.append((np.mean(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['angle'].iloc[0])*np.pi/180)/np.sqrt(len(data_df[(data_df['K_r'] == k_r) & (data_df['K_theta'] == k_t)]['angle'].iloc[0])))
+            mean_t.append(np.mean(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['angle_deg'].iloc[0])*np.pi/180)
+            sem_t.append((np.mean(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['angle_deg'].iloc[0])*np.pi/180)/np.sqrt(len(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['angle_deg'].iloc[0])))
+
+            mean_blen.append(np.max(np.array(data_df[(data_df['K_r phys'] == k_r) & (data_df['K_theta phys'] == k_t)]['bond lengths'].iloc[0]), axis=1))
 
         except ValueError as e:
             print(f"Error calculating std for k_r={k_r}, k_t={k_t}: {e}")
-            std_r.append(np.nan)
-            sem_std_r.append(np.nan)
+            std_mlen.append(np.nan)
+            sem_std_mlen.append(np.nan)
 
-            mean_r.append(np.nan)
-            mean_r.append(np.nan)
+            mean_mlen.append(np.nan)
+            mean_mlen.append(np.nan)
 
             mean_t.append(np.nan)
             sem_t.append(np.nan)
@@ -239,33 +250,36 @@ def stats(data_df):
     print(f'k_r: {rt_pairs[delta_t.argmin()][0]}, k_t: {rt_pairs[delta_t.argmin()][1]}')
 
     #k_r = data_df['K_r'].iloc[np.where(delta_t < 0.001)]
-    #k_t = data_df['K_theta'].iloc[np.where(delta_t < 0.001)]
+    #k_t = data_df['K_theta phys'].iloc[np.where(delta_t < 0.001)]
 
     #corr = np.corrcoef((sigma_r), np.log(sigma_t))
     #print("Correlation coefficient:", corr)
 
     monomer_df = pd.DataFrame({
-        "K_r": data_df['K_r'],
-        "K_theta": data_df['K_theta'],
+        "K_r": data_df['K_r phys'],
+        "K_theta": data_df['K_theta phys'],
 
-        "r mean": mean_r,
-        "r sem": sem_r,
-        "r std": std_r,
-        "r std sem": sem_std_r,
+        "monomer length mean": mean_mlen,
+        "monomer length sem": sem_mlen,
+        "monomer length std": std_mlen,
+        "monomer length std sem": sem_std_mlen,
 
         "theta mean": mean_t_arr,
         "theta sem": sem_t,
         "theta std": std_t,
         "theta std sem": sem_std_t,
 
-        "lengths": data_df['lengths'].values,
-        "angles": data_df['angle'].values,
+        "blen mean": mean_blen,
+
+        "lengths": data_df['monomer lengths'].values,
+        "angles": data_df['angle_deg'].values,
     })
+    
     return monomer_df 
 
 def main():
     data_df = readfile()
-    data_df.sort_values(by=['K_r', 'K_theta'], inplace=True)
+    data_df.sort_values(by=['K_r phys', 'K_theta phys'], inplace=True)
     
     monomer_df = stats(data_df)
 
@@ -275,49 +289,45 @@ def main():
     os.chdir(path)
     print(os.getcwd())
 
-    plt.figure(1)
-    plt.ylim(0,5)
-    x = np.linspace(0.01, 20, 200)
+    
+    x = np.linspace(0.005, 45, 300)
     p = np.sqrt(np.array([VarR(i) for i in x]))
-    print('sigma---------------------')
-    print(p)
     l = (np.array([ExpectedR(i) for i in x]))
-    print('<R>-----------------------')
-    print(l)
     y = p/l
-    plt.plot(x, l, label='theory <R>')
-    plt.errorbar(
-        monomer_df['K_r'],
-        ((monomer_df['r mean'])),
-        yerr=(monomer_df['r sem']),
-        color='black',
-        fmt='.',   
-        markersize=1,
-        capsize=2,
-        capthick=0.5,
-        elinewidth=0.3
-    )
-    plt.scatter(
-        monomer_df['K_r'],
-        (monomer_df['r mean']),
-        c=monomer_df['K_theta'],
-        s=10
-    )
-    plt.colorbar(label='K_theta')
-    plt.xlabel('K_r')
-    plt.ylabel('<R> (lj)')
-    plt.title('Expected monomer length coloured by K_theta (logxy)')
-    plt.legend()
 
+    # plt.figure(1)
+    # plt.ylim(0,4)
+    # plt.plot(x, l, label='theory <R>')
+    # plt.errorbar(
+    #     monomer_df['K_r'],
+    #     ((monomer_df['monomer length mean'])),
+    #     yerr=(monomer_df['monomer length sem']),
+    #     color='black',
+    #     fmt='.',   
+    #     markersize=1,
+    #     capsize=2,
+    #     capthick=0.5,
+    #     elinewidth=0.3
+    # )
+    # plt.scatter(
+    #     monomer_df['K_r'],
+    #     (monomer_df['monomer length mean']),
+    #     c=monomer_df['K_theta'],
+    #     s=10
+    # )
+    # plt.colorbar(label='K_theta')
+    # plt.xlabel('K_r')
+    # plt.ylabel('<R> (lj)')
+    # plt.title('Expected monomer length coloured by K_theta')
+    # plt.legend()
 
     plt.figure(2)
-    plt.ylim(0,5)
-    plt.plot(x, l, label='theory std')
+    plt.plot(x, y, label='theory std/length')
 
     plt.errorbar(
         monomer_df['K_r'],
-        ((monomer_df['r std'])),
-        yerr=(monomer_df['r std sem']),
+        monomer_df['monomer length std']/monomer_df['monomer length mean'],
+        yerr=monomer_df['monomer length std']/monomer_df['monomer length mean']*np.sqrt(monomer_df['monomer length std sem']**2/monomer_df['monomer length std']**2 + monomer_df['monomer length sem']**2/monomer_df['monomer length mean']**2),
         color='black',
         fmt='.',   
         markersize=1,
@@ -327,20 +337,19 @@ def main():
     )
     plt.scatter(
         monomer_df['K_r'],
-        (monomer_df['r std']),
+        monomer_df['monomer length std']/monomer_df['monomer length mean'],
         c=monomer_df['K_theta'],
-        s=10
+        s=2
     )
     plt.colorbar(label='K_theta')
     plt.xlabel('K_r')
-    plt.ylabel('r std (lj)')
-    plt.title('std of monomer length coloured by K_theta (logxy)')
-    plt.legend()
-
+    plt.ylabel('Monomer length std (lj)')
+    plt.title('std of monomer length coloured by K_theta')
     plt.legend()
    
     plt.figure(3)
-    x = np.linspace(0.1, 40, 200)
+    plt.ylim(0,4)
+    x = np.linspace(0.1, 60, 400)
     y = np.array([ExpectedTheta(i) for i in x])
     plt.plot(x, (y[:,0]), color='red')
 
@@ -364,9 +373,36 @@ def main():
     plt.colorbar(label='K_r')
     plt.xlabel('K_theta')
     plt.ylabel('Mean theta (rad)')
-    plt.title('Mean theta coloured by K_r (logxy)')
-    plt.legend()
+    plt.title('Mean theta coloured by K_r')
+
+    plt.figure(6)
+    plt.title('Single bond length histogram at k_theta = 20.0 lj. Timestep=0.001, Count=800000, 5 seeds')
+    x = np.linspace(0,4,200)
+    r = np.array([Zr_bond(i, 45) for i in x])
+    Z, err = integrate.quad(Zr_bond, 0, np.inf, args=45)
+    plt.hist(data_df[(data_df['K_r phys'] == 45) & (data_df['K_theta phys'] == 20.0)]['bond lengths'].iloc[0][0,:], bins=200, density=True, alpha=0.5, label='k_r = 45 lj')
+    plt.plot(x, r/Z, label='k_r = 45 lj theory')
+    r = np.array([Zr_bond(i, 5) for i in x])
+    Z, err = integrate.quad(Zr_bond, 0, np.inf, args=5)
+    plt.hist(data_df[(data_df['K_r phys'] == 5) & (data_df['K_theta phys'] == 20.0)]['bond lengths'].iloc[0][0,:], bins=200, density=True, alpha=0.5, label='k_r = 5 lj')
+    plt.plot(x, r/Z, label='k_r = 5 lj theory')
+    plt.xlabel('Bond length (lj)')
+    plt.ylabel('Frequency')
+
+    plt.figure(7)
+    plt.title('Monomer angle histogram at k_r = 45 lj. Timestep=0.001, Count=800000, 5 seeds')
+    x = np.linspace(0,np.pi,200)
+    r = np.array([ZTheta(i, 20) for i in x])
+    Z, err = integrate.quad(ZTheta, 0, np.pi, args=20)
+    plt.hist(monomer_df[(monomer_df['K_r'] == 45) & (monomer_df['K_theta'] == 20.0)]['angles'].iloc[0]*np.pi/180, bins=200, density=True, alpha=0.5, label='k_theta = 20 lj')
+    plt.plot(x, r/Z, label='k_theta = 20 lj theory')
+    r = np.array([ZTheta(i, 2) for i in x])
+    Z, err = integrate.quad(ZTheta, 0, np.pi, args=2)
+    plt.hist(monomer_df[(monomer_df['K_r'] == 45) & (monomer_df['K_theta'] == 2.0)]['angles'].iloc[0]*np.pi/180, bins=200, density=True, alpha=0.5, label='k_theta = 2.0 lj')
+    plt.plot(x, r/Z, label='k_theta = 2.0 lj theory')
+    plt.xlabel('Monomer angle (rad)')
+    plt.ylabel('Frequency')
+
     plt.show()
-    # monomer_df.to_csv('corr_data.csv')
 
 main()
