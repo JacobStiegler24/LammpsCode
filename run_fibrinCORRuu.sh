3f568bbdc86a9d919cff5c0b9862f45e97547549
JacobStiegler24 <jacob.stiegler@gmail.com>
2025-08-13 09:21:32 +0100

    Added FENE potetntial for unfolding, analysis added in bondreaderCorrUU.

---
+++ run_fibrinCORRuu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Set bond types to 2 and disable reactions
+
+#=======================================
+# USER SETTINGS
+
+lmpinput="fibrin-monomer.lam"
+stretch_force=0.0 # must be decimal values. 
+#| xargs -n1 printf "%.1f\n" from chatgpt to make forces a decimal for all increments.
+seed="54654651 54654653 54654659 54654661 54654685"
+k_r_arr=$(seq 1 5 31 | xargs -n1 printf "%.1f\n")
+k_theta=7.785
+k_r=4.1535
+
+#=======================================
+
+ 
+#=======================================
+rm -rf output/CORRuu
+#=======================================
+
+
+# Run simulations
+for seedval in $seed ; do
+  for k_r_prime in $k_r_arr ; do
+      outdir="output/CORRuu/k_r_prime${k_r_prime}_Seed${seedval}"
+      mkdir -p "$outdir"
+      ./lmp_mpi \
+        -var stretch_force "$stretch_force" \
+        -var seed "$seedval" \
+        -var K_r_prime "$k_r_prime" \
+        -var K_r "$k_r"\
+        -var K_theta "$k_theta" \
+        -var outdir "$outdir" \
+        < "$lmpinput"
+  done
+done
